62cb01e185fb680afc1855eb13372f784070778b
= <=>
2016-05-05 03:41:35 +0300

    Added a Register Patient Class to the Project

---
+++ src/hospital/RegisterPatient.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital;
+
+import javax.swing.JDialog;
+
+/**
+ *
+ * @author Joseph
+ */
+public class RegisterPatient extends JDialog{
+    
+}
